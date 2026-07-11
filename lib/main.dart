fe8015559b23d7d3ab546a71635258062626ad98
AhmedKhedr <ahmedkhedrx1@gmail.com>
2026-07-11 13:18:39 +0300

    fake push to orgin development

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:doc_point/core/routing/app_router.dart';
 import 'package:doc_point/doc_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() {  
+  
   runApp(DocApp(appRouter: AppRouter()));
 }
